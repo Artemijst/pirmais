31a68149c7fecabd95de5c5226768cd58d20ae13
Artemijs <artemijs1337@gmail.com>
2016-08-24 17:40:43 +0300

    first commit

---
+++ App1/App1/otralapa.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace App1
+{
+    public partial class otralapa : ContentPage
+    {
+        public otralapa()
+        {
+            InitializeComponent();
+        }
+
+        private void button_Onclicked1(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new Page1());
+        }
+    }
+}
